fde8c52038dc1135d30242a8ecb7a539cf7d2bb6
thadmuchnok64 <tmuchnok@ufl.edu>
2026-07-17 03:40:04 -0400

    shpatghetti

--- Scripts/Entities/Enemies/IHE_Idle.cs
+++ Scripts/Entities/Enemies/IHE_Idle.cs
@@ -7,18 +7,26 @@ public partial class IHE_Idle: EnemyState
 	[Export] EnemyState seekingState;
 	[Export] EnemyState recoilState;
     [Export] EnemyState postureBreakState;
+	[Export] IHE_Attack attackState;
+	[Export] float distanceToFollow = 3f;
+	[Export] float maxTurnPerSec = 1f;
 
 
 
 
-    public override EnemyState Enter(Enemy enemy)
+
+	public override EnemyState Enter(Enemy enemy)
 	{
 		return base.Enter(enemy);
 	}
 
 	public override EnemyState DetectPlayerEvent()
 	{
-		return seekingState;
+		if (enem.GlobalPosition.DistanceTo(GameMaster.Instance.GetPlayer().GlobalPosition) >= distanceToFollow)
+		{
+			return seekingState;
+		}
+		return null;
 
 	}
 
@@ -33,4 +41,24 @@ public partial class IHE_Idle: EnemyState
             return recoilState;
         }
     }
+
+	public override EnemyState PhysicsProcess(double delta)
+	{
+		float desiredAngle = enem.meshRoot.Basis.Z.SignedAngleTo((GameMaster.Instance.GetPlayer().GlobalPosition - enem.meshRoot.GlobalPosition).Normalized(), Vector3.Up);
+		float clampedAngle = Mathf.Clamp(desiredAngle, -(float)delta * maxTurnPerSec * Mathf.Pi, (float)delta * maxTurnPerSec * Mathf.Pi);
+		enem.meshRoot.RotateY(clampedAngle);
+		return base.PhysicsProcess(delta);
+
+	}
+
+	public override EnemyState Process(double delta)
+	{
+		if (attackState.IsAttackValid())
+			return attackState;
+		if (enem.GlobalPosition.DistanceTo(GameMaster.Instance.GetPlayer().GlobalPosition) >= distanceToFollow)
+		{
+			return seekingState;
+		}
+		return base.Process(delta);
+	}
 }
--- Scripts/Entities/Enemies/IHE_Shamble.cs
+++ Scripts/Entities/Enemies/IHE_Shamble.cs
@@ -8,7 +8,7 @@ public partial class IHE_Shamble : EnemyState
 	[Export] EnemyState recoilState;
 	[Export] EnemyState postureBreakState;
 	[Export] IHE_Attack attackState;
-	[Export] float distanceToAttack = 2f;
+	[Export] float optimalDistance = 2f;
 	[Export] float maxTurnPerSec = 90f;
     public override EnemyState Enter(Enemy enemy)
 	{
@@ -19,23 +19,15 @@ public partial class IHE_Shamble : EnemyState
 	}
 	public override EnemyState PhysicsProcess(double delta)
 	{
-		//enem.cb.Velocity = (enem.nav.GetNextPathPosition() - enem.cb.GlobalPosition).Normalized() * speed;
-		//enem.cb.MoveAndSlide();
-		//enem.cb.Rotate();+
+
 		float desiredAngle = enem.meshRoot.Basis.Z.SignedAngleTo((enem.nav.GetNextPathPosition() - enem.meshRoot.GlobalPosition).Normalized(), Vector3.Up);
-		//float frameAngle = desiredAngle
 		float clampedAngle = Mathf.Clamp(desiredAngle, -(float)delta * maxTurnPerSec*Mathf.Pi, (float)delta * maxTurnPerSec * Mathf.Pi);
 		enem.meshRoot.RotateY(clampedAngle);
 		enem.cb.Velocity = enem.meshRoot.Basis.Z* speed;
         enem.cb.MoveAndSlide();
-        //if (enem.cb.Velocity.Length() > .2f)
-        //{
-        //enem.meshRoot.LookAt(enem.cb.Position - enem.cb.Velocity.Normalized() * 5, Vector3.Up);
-        //enem.meshRoot.Rotation = new Vector3(0, enem.meshRoot.Rotation.Y, 0);
-        //}
 
 
-        if (enem.nav.TargetPosition.DistanceTo(enem.cb.GlobalPosition) < .7f)
+        if (enem.GlobalPosition.DistanceTo(GameMaster.Instance.GetPlayer().GlobalPosition) < optimalDistance)
 		{
 			return idleState;
 		}
@@ -62,7 +54,7 @@ public partial class IHE_Shamble : EnemyState
     {
 		if (attackState.IsAttackValid())
 			return attackState;
-		if (enem.nav.TargetPosition.DistanceTo(GameMaster.Instance.GetPlayer().GlobalPosition) > distanceToAttack)
+		if (enem.nav.TargetPosition.DistanceTo(GameMaster.Instance.GetPlayer().GlobalPosition) > optimalDistance)
 		{
             enem.nav.TargetPosition = GameMaster.Instance.GetPlayer().Position;
 
